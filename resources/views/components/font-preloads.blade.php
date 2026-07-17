5a6bd5024eef78c54b8879e65cf6e09a9d76351b
ahmed elbardawell <brdweelahmed@gmail.com>
2026-07-17 10:09:14 +0300

    fix: font-preloads Blade syntax error (newlines between directives)

--- resources/views/components/font-preloads.blade.php
+++ resources/views/components/font-preloads.blade.php
@@ -4,4 +4,12 @@
 @foreach($wList as $w)
     <link rel="preload" as="font" type="font/woff2" crossorigin href="/fonts/instrument-sans-{{ $w }}.woff2">
 @endforeach
-<style>@foreach($wList as $w)@font-face{font-family:'Instrument Sans';font-style:normal;font-weight:{{ $w }};font-stretch:100%;font-display:swap;src:url('/fonts/instrument-sans-{{ $w }}.woff2') format('woff2')}@endforeach@if($withPlayfair)@font-face{font-family:'Playfair Display';font-style:normal;font-weight:400;font-stretch:100%;font-display:swap;src:url('/fonts/playfair-display-400.woff2') format('woff2')}@font-face{font-family:'Playfair Display';font-style:normal;font-weight:700;font-stretch:100%;font-display:swap;src:url('/fonts/playfair-display-700.woff2') format('woff2')}@endif</style>
+<style>
+@foreach($wList as $w)
+@font-face{font-family:'Instrument Sans';font-style:normal;font-weight:{{ $w }};font-stretch:100%;font-display:swap;src:url('/fonts/instrument-sans-{{ $w }}.woff2') format('woff2')}
+@endforeach
+@if($withPlayfair)
+@font-face{font-family:'Playfair Display';font-style:normal;font-weight:400;font-stretch:100%;font-display:swap;src:url('/fonts/playfair-display-400.woff2') format('woff2')}
+@font-face{font-family:'Playfair Display';font-style:normal;font-weight:700;font-stretch:100%;font-display:swap;src:url('/fonts/playfair-display-700.woff2') format('woff2')}
+@endif
+</style>
